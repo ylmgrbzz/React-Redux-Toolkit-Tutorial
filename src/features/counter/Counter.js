6822727fc1be603929baadf0b6a622472ae52f05
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-17 20:53:33 +0300

    Add input field and button to set custom increment amount in Counter component

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -5,6 +5,8 @@ import { decrement, increment, reset, incrementByAmount } from "./CounterSlice";
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = React.useState("0");
+  const addAmount = Number(incrementAmount) || 0;
 
   return (
     <section>
@@ -16,6 +18,14 @@ const Counter = () => {
         <button onClick={() => dispatch(reset())}>Reset</button>
         <button onClick={() => dispatch(incrementByAmount(5))}>
           Increment by 5
+        </button>
+        <input
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button onClick={() => dispatch(incrementByAmount(addAmount))}>
+          Add Amount
         </button>
       </div>
     </section>
