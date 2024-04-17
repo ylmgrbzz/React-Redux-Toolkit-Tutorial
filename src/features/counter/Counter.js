cb5c5a3968043a9ade39ae311488e1ea9b2fb2a3
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-17 20:51:01 +0300

    Add reset and incrementByAmount functionality to Counter component

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./CounterSlice";
+import { decrement, increment, reset, incrementByAmount } from "./CounterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
@@ -13,6 +13,10 @@ const Counter = () => {
         <button onClick={() => dispatch(decrement())}>-</button>
         <span>{count}</span>
         <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => dispatch(incrementByAmount(5))}>
+          Increment by 5
+        </button>
       </div>
     </section>
   );
