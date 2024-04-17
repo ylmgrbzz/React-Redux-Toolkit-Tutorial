32f7515ae55274b192fdc57def76c38bba340b2e
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-17 21:43:01 +0300

    Add date-fns library for handling date and time in package.json and package-lock.json

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,12 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
     id: "1",
     title: "Learning Redux Toolkit",
     content: "I've heard good things.",
-    // date: sub(new Date(), { minutes: 10 }).toISOString(),
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
     reactions: {
       thumbsUp: 0,
       wow: 0,
@@ -19,7 +20,7 @@ const initialState = [
     id: "2",
     title: "Slices...",
     content: "The more I say slice, the more I want pizza.",
-    // date: sub(new Date(), { minutes: 5 }).toISOString(),
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
     reactions: {
       thumbsUp: 0,
       wow: 0,
@@ -44,6 +45,7 @@ const postSlice = createSlice({
           title,
           content,
           userId,
+          date: new Date().toISOString(),
         },
       };
     },
