39364eaf0661752a09c8bb11d6978718dc4ec2e4
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-17 20:57:01 +0300

    Add postsSlice.js file for managing posts state

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/CounterSlice";
+import postReducer from "../features/posts/postsSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    posts: postReducer,
   },
 });
