761f0c93f87eb47cc2c4d8def3e37d19c8dc4520
YALIM GÜRBÜZ <99664429+ylmgrbzz@users.noreply.github.com>
2024-04-23 15:34:12 +0300

    initialState

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,47 +2,53 @@ import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning Redux Toolkit",
-    content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: "2",
-    title: "Slices...",
-    content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: "3",
-    title: "Learning Redux Toolkit333",
-    content: "I've heard good things.333",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
+// const initialState = [
+//   {
+//     id: "1",
+//     title: "Learning Redux Toolkit",
+//     content: "I've heard good things.",
+//     date: sub(new Date(), { minutes: 10 }).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       wow: 0,
+//       heart: 0,
+//       rocket: 0,
+//       coffee: 0,
+//     },
+//   },
+//   {
+//     id: "2",
+//     title: "Slices...",
+//     content: "The more I say slice, the more I want pizza.",
+//     date: sub(new Date(), { minutes: 5 }).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       wow: 0,
+//       heart: 0,
+//       rocket: 0,
+//       coffee: 0,
+//     },
+//   },
+//   {
+//     id: "3",
+//     title: "Learning Redux Toolkit333",
+//     content: "I've heard good things.333",
+//     date: sub(new Date(), { minutes: 10 }).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       wow: 0,
+//       heart: 0,
+//       rocket: 0,
+//       coffee: 0,
+//     },
+//   },
+// ];
+
+const initialState = {
+  posts: [],
+  status: "idle",
+  error: null,
+};
 
 const postSlice = createSlice({
   name: "posts",
